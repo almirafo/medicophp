d8855e3ebc6dcfda1ecf7d1d4a3076018d10c36d
Almir Fontenele <almir.oliveira@supportcomm.com.br>
2017-03-10 17:21:08 -0300

    implementação de salvar e buscar js

--- scripts/app/agendamento.js
+++ scripts/app/agendamento.js
@@ -7,12 +7,25 @@
 
 var app = angular.module("agendamentoApp",[]);
 
-app.controller('agendamentoCtrl',function ($scope,$http){
+app.controller('agendamentoCtrl',function ($scope,$http,$window){
     $scope.paciente =[]; 
     
+    
     $scope.salvar = function(){
-        
     	
-        $http.get('pacientes.php')
-    }
+    	$http.post("http://localhost:90/medico/pacienteAPI.php/salvar").then(
+    		function(response){
+    			$scope.status = "salvo com sucesso";
+    		}	
+    	);
+    	
+    	 $window.location.href = 'pacientes.php';
+    };
+    
+    
+    $scope.buscar =  function(id){
+    	$http.get("http://localhost:90/medico/pacienteAPI.php/buscar/"+id).then(function(response) {
+    		$scope.paciente = response.data;
+    	});
+    };
 });
