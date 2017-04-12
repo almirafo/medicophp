bd28908a64688bf67deffa48c545cfd24134ad7a
almir <almirafo@gmail.com>
2017-04-11 21:46:40 -0300

    ligacoes

---
+++ scripts/app/ligacoes.js
@@ -0,0 +1,72 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+'use strict';
+
+var app = angular.module("ligacoesApp", ['angularUtils.directives.dirPagination', 
+                                         'ngRoute']);
+
+
+app.filter('propsFilter', function() {
+  return function(items, props) {
+    var out = [];
+    if (angular.isArray(items)) {
+      items.forEach(function(item) {
+        var itemMatches = false;
+        var keys = Object.keys(props);
+        for (var i = 0; i < keys.length; i++) {
+          var prop = keys[i];
+          var text = props[prop].toLowerCase();
+          if (item[prop].toString().toLowerCase().indexOf(text) !== -1) {
+            itemMatches = true;
+            break;
+          }
+        }
+        if (itemMatches) {
+          out.push(item);
+        }
+      });
+    } else {
+      // Let the output be the input untouched
+      out = items;
+    }
+    return out;
+  }
+});
+
+
+
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
+
+app.controller('ligacoesCtrl', function ($scope, $http, $timeout, $interval) {
+    
+     $scope.ligacoes          = {};
+     
+     
+     $http.get("http://localhost:90/medico/api/ligacoesAPI.php?action=listar")
+     .then(
+           function(result){
+                   $scope.ligacoess= result.data;
+     }) ;
+    
+    
+    
+    
+    
+});
